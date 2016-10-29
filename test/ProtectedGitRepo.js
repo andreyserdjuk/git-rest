2a8951fda53594a0898cb6b04a7b1e1102975908
andreyserdjuk <ase6120@gmail.com>
2016-10-29 22:09:32 +0300

    ProtectedGitRepo test coverage

--- test/ProtectedGitRepo.js
+++ test/ProtectedGitRepo.js
@@ -1,5 +1,20 @@
 "use strict";
+const util_1 = require('util');
+const assert = require('assert');
+const ProtectedGitRepo_1 = require('./../ProtectedGitRepo');
 describe('ProtectedGitRepo', () => {
-    it('getRepo: ', () => {
+    let availablePaths = new Map();
+    availablePaths.set('path1', 'path/to/path1');
+    let repo = new ProtectedGitRepo_1.ProtectedGitRepo(availablePaths);
+    it('isAvailable()', () => {
+        assert.ok(repo.isAvailable('path1'));
+    });
+    it('getAvailablePaths()', () => {
+        assert.equal(1, repo.getAvailablePaths().size);
+    });
+    it('getRepo()', () => {
+        util_1.debug;
+        let git = repo.getRepo('path1');
+        assert.ok(typeof git.branch === 'function');
     });
 });
--- test/ProtectedGitRepo.ts
+++ test/ProtectedGitRepo.ts
@@ -1,6 +1,25 @@
+import * as console from 'console';
+import { debug } from 'util';
+import GitFactory = require('simple-git');
 import assert = require('assert');
+import {ProtectedGitRepo} from './../ProtectedGitRepo';
 
 describe('ProtectedGitRepo', () => {
-  it('getRepo: ', () => {
+  let availablePaths = new Map<string, string>();
+  availablePaths.set('path1', 'path/to/path1');
+  let repo = new ProtectedGitRepo(availablePaths);
+
+  it('isAvailable()', () => {
+    assert.ok(repo.isAvailable('path1'));
+  });
+
+  it('getAvailablePaths()', () => {
+    assert.equal(1, repo.getAvailablePaths().size);
+  });
+
+  it('getRepo()', () => {
+    debug;
+    let git = repo.getRepo('path1');
+    assert.ok(typeof git.branch === 'function');
   });
 });
